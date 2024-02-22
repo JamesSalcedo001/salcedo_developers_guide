7c3f029f83a22355e7ec22f5b7810533b43fe3f0
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-02-21 23:45:22 -0600

    added home component

--- client/src/slices/userSlice.jsx
+++ client/src/slices/userSlice.jsx
@@ -5,16 +5,19 @@ export const userSlice = createSlice({
     initialState: {
         user: null,
         loggedIn: false,
+        admin: false,
         errors: []
     },
     reducers: {
         login: (state, action) => {
             state.user = action.payload
             state.loggedIn = true
+            state.admin = action.payload.admin
         },
         logout: (state) => {
             state.user = null
             state.loggedIn = false
+            state.admin = false
         },
         setErrors: (state, action) => {
             state.errors = action.payload
